1832e9b6098e939c76aa050b02476fb843f9f7a0
Nicolás Correa <ncorreaa@falabella.cl>
2024-10-28 20:37:54 -0300

    second week features

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,53 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  user = '';
+  name = '';
+  lastName = '';
+  educationLevel = '';
+  birthDate = '';
+  isCleaned = false;
 
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private alertController: AlertController
+  ) {}
+
+  ngOnInit(){
+    this.route.queryParams.subscribe(params => {
+      this.user = params['user'];
+    });
+  }
+
+  async showInfo(){
+    if (!this.name || !this.lastName){
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'El campo nombre y apellido son requeridos',
+        buttons: ['OK']
+      })
+      await alert.present();
+      return;
+    }
+    const alert = await this.alertController.create({
+      header: 'Información',
+      message: `Su nombre es ${this.name} ${this.lastName}`,
+      buttons: ['OK']
+    })
+    await alert.present();
+  }
+
+  clearInputs(){
+    this.name = '';
+    this.lastName = '';
+    this.educationLevel = '';
+    this.birthDate = '';
+
+    this.isCleaned = true;
+
+    setTimeout(() => {
+      this.isCleaned = false;
+    }, 1000);
+  }
 
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  user = '';
+  password = '';
 
-  constructor() { }
+  constructor(
+    private navCtrl: NavController,
+    private alertController: AlertController
+  ) { }
 
   ngOnInit() {
+  }
+
+  async showAlert(message: string){
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message,
+      buttons: ['OK']
+    })
+    await alert.present();
+  }
+
+  login(){
+
+    const isValid = this.loginValidation();
+    if(!isValid) return;
+    this.navCtrl.navigateForward(['/home'],{
+      queryParams: {
+        user: this.user,
+        password: this.password
+      }
+    })
+  }
+
+  loginValidation(): boolean{
+    if(!this.user){
+      this.showAlert('El usuario es requerido');
+      return false;
+    }
+    if(this.user.length < 3 || this.user.length > 8){
+      this.showAlert('El user debe tener entre 3 y 8 caracteres');
+      return false;
+    }
+    if(!this.password){
+      this.showAlert('La contraseña es requerida');
+      return false;
+    }
+    if(this.password.length != 4){
+      this.showAlert('La contraseña debe tener 4 caracteres');
+      return false;
+    }
+    return true;
   }
 
 }
