ca49d64761b066d552c7bc722424ccf31950c390
Nicolás Correa <ncorreaa@falabella.cl>
2024-11-23 15:23:02 -0300

    add camera functionality

--- src/app/tabs/profile/profile.page.ts
+++ src/app/tabs/profile/profile.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProfileImageApiService } from '../../service/profile-image-api.service';
 import { DomSanitizer } from '@angular/platform-browser';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-profile',
@@ -12,6 +13,7 @@ export class ProfilePage implements OnInit {
   name: string = '';
   lastname: string = '';
   profileImage: any;
+  // customPhoto: string | undefined = '';
 
   constructor(
     private profileImageApiService: ProfileImageApiService,
@@ -23,11 +25,27 @@ export class ProfilePage implements OnInit {
     this.name = localStorage.getItem('name') || '';
     this.lastname = localStorage.getItem('lastname') || '';
 
+    if (this.profileImage === undefined) {
     this.profileImageApiService
       .getProfileImage(this.username)
       .subscribe((blob) => {
         const objectURL = URL.createObjectURL(blob);
         this.profileImage = this.sanitizer.bypassSecurityTrustUrl(objectURL);
       });
+    }
+  }
+
+  async takePicture () {
+    try{
+      const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Camera
+      });
+      this.profileImage = image.dataUrl;
+    } catch (error) {
+      alert(error);
+    }
   }
 }
