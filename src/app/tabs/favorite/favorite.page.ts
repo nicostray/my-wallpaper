2aeb3236b2343aea8f29b02e69d968dc62946dcc
Nicolás Correa <ncorreaa@falabella.cl>
2024-12-13 20:03:22 -0300

    favorite page finished

--- src/app/tabs/favorite/favorite.page.ts
+++ src/app/tabs/favorite/favorite.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChildren } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ImageItemComponent } from '../../components/image-item/image-item.component';
 
 @Component({
   selector: 'app-favorite',
@@ -6,11 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorite.page.scss'],
 })
 export class FavoritePage implements OnInit {
-
-  constructor() { }
+  favoriteList = this.getFavorites();
+    @ViewChildren(ImageItemComponent) imageItems!: QueryList<ImageItemComponent>;
+  constructor(
+    private navCtrl: NavController
+  ) { }
 
   ngOnInit() {
+    this.favoriteList = this.getFavorites();
+  }
+
+  ionViewWillEnter() {
+    if (this.imageItems) {
+      this.imageItems.forEach((imageItem) => {
+        imageItem.checkFavorite();
+      });
+    }
+    this.favoriteList = this.getFavorites();
   }
 
   iterations = Array(3).fill(0);
+
+  getFavorites() {
+    let favorites = localStorage.getItem('favorites');
+    if (favorites) {
+      return JSON.parse(favorites);
+    }
+    return [];
+  }
+  handleImage = (item: any) => {
+    this.navCtrl.navigateForward(`/tabs/image-viewer?imageId=${item.id}&imageName=${item.name}`);
+  }
 }
